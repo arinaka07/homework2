574e07b412dbed430ca6137bfb04ea2358620c61
Arina <arina.kostina07@mail.ru>
2026-09-20 22:27:47 +0300

    геттеры/сеттеры/print

--- 001.cpp
+++ 001.cpp
@@ -19,6 +19,15 @@ class AlgebraComplex : public Complex
     AlgebraComplex();//конструктор по умолчанию
     AlgebraComplex(double Re, double Im);
 
+    //геттеры
+    double getRe() const;
+    double getIm() const;
+    //сеттеры
+    void setRe(double);
+    void setIm(double);
+
+    void print() const override;//функция из class comlex
+
 };
 
 AlgebraComplex::AlgebraComplex() : Re(0.0), Im(0.0) 
@@ -29,6 +38,28 @@ AlgebraComplex::AlgebraComplex(double Re, double Im) : Re(Re), Im(Im)
 {
 
 }
+
+double AlgebraComplex::getRe() const
+{
+    return this->Re;
+}
+double AlgebraComplex::getIm() const
+{
+    return this-> Im;
+}
+void AlgebraComplex::setRe(double Re)
+{
+    this->Re = Re;
+}
+void AlgebraComplex::setIm(double Im)
+{
+    this->Im = Im;
+}
+void AlgebraComplex::print() const
+{
+    printf("%f + %fi", Re, Im);
+}
+
 
 int main()
 {
