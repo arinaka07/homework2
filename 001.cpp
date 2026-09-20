2c0a516991f47acb506ef1515591107ad659bfbd
Arina <arina.kostina07@mail.ru>
2026-09-20 22:33:50 +0300

    подведено к примеру из файла

--- 001.cpp
+++ 001.cpp
@@ -1,63 +1,69 @@
 #include <cstdio>
 #include <cstdlib>
+#include <iostream>
 
-//абстрактный класс 
+using std::cin, std::cout, std::endl, std::ostream;
 
-class Complex {
-    public:
-    virtual void print() = 0;
-};
-
-//алгебраический вид
-class AlgebraComplex : public Complex
+namespace my_scope
 {
-    private:
-    double Re;
-    double Im;
+    //абстрактный класс 
 
-    public:
-    AlgebraComplex();//конструктор по умолчанию
-    AlgebraComplex(double Re, double Im);
+    class Complex {
+        public:
+        virtual void print() const = 0;
+    };
 
-    //геттеры
-    double getRe() const;
-    double getIm() const;
-    //сеттеры
-    void setRe(double);
-    void setIm(double);
+    //алгебраический вид
+    class AlgebraComplex : public Complex
+    {
+        private:
+        double Re;
+        double Im;
 
-    void print() const override;//функция из class comlex
+        public:
+        AlgebraComplex();//конструктор по умолчанию
+        AlgebraComplex(double Re, double Im);
 
-};
+        //геттеры
+        double getRe() const;
+        double getIm() const;
+        //сеттеры
+        void setRe(double);
+        void setIm(double);
 
-AlgebraComplex::AlgebraComplex() : Re(0.0), Im(0.0) 
-{
+        void print() const override;//функция из class comlex
 
-}
-AlgebraComplex::AlgebraComplex(double Re, double Im) : Re(Re), Im(Im)
-{
+    };
 
-}
+    AlgebraComplex::AlgebraComplex() : Re(0.0), Im(0.0) 
+    {
 
-double AlgebraComplex::getRe() const
-{
-    return this->Re;
-}
-double AlgebraComplex::getIm() const
-{
-    return this-> Im;
-}
-void AlgebraComplex::setRe(double Re)
-{
-    this->Re = Re;
-}
-void AlgebraComplex::setIm(double Im)
-{
-    this->Im = Im;
-}
-void AlgebraComplex::print() const
-{
-    printf("%f + %fi", Re, Im);
+    }
+    AlgebraComplex::AlgebraComplex(double Re, double Im) : Re(Re), Im(Im)
+    {
+
+    }
+
+    double AlgebraComplex::getRe() const
+    {
+        return this->Re;
+    }
+    double AlgebraComplex::getIm() const
+    {
+        return this-> Im;
+    }
+    void AlgebraComplex::setRe(double Re)
+    {
+        this->Re = Re;
+    }
+    void AlgebraComplex::setIm(double Im)
+    {
+        this->Im = Im;
+    }
+    void AlgebraComplex::print() const
+    {
+        cout << Re << " + " << Im << "i";
+    }
 }
 
 
